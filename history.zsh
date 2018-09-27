155f448bb046e72d1aa649da8d0cdbcfb60514b9
Naoto Kaneko <naoty.k@gmail.com>
2018-09-27 15:27:56 +0900

    Add -E 1 arg to show all histories

--- history.zsh
+++ history.zsh
@@ -1,5 +1,5 @@
 fzf_history() {
-    local cmd="$(history | fzf --reverse | cut -c 8-)"
+    local cmd="$(history -E 1 | fzf --reverse | cut -c 8-)"
     echo $cmd
     eval $cmd
     zle reset-prompt
